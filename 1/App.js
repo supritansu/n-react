e876107f0ff143eb7467b6818aaffe4738d9a435
supritansu <suprit.ansu@gmail.com>
2023-09-27 03:24:04 +0530

    Add files via upload

---
+++ 1/App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", { id: "parent" },
+    [React.createElement("h1", { id: "children" }, [
+        React.createElement("h2", { id: "gchild" }, ["we are grandchildren"]), React.createElement("h2", { id: "gchild" }, ["we are grandchildren"])
+    ]), React.createElement("h1", { id: "children" },
+        [React.createElement("h2", { id: "gchild" }, ["we are grandchildren"]), React.createElement("h2", { id: "gchild" }, ["we are grandchildren"])])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
