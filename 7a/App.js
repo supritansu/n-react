1c98bc0f45e83707b948397f8d10bf44a02fa67a
supritansu <suprit.ansu@gmail.com>
2023-10-03 09:24:06 +0530

    Add files via upload

---
+++ 7a/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Header } from "./src/components/Header";
+import { Body } from "./src/components/Body";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./src/components/About";
+import Error from "./src/components/Error";
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Outlet />
+        </div>
+    );
+};
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/about",
+                element: <About />,
+
+
+            }, {
+                path: "/",
+                element: <Body />,
+
+
+            }],
+        errorElement: <Error />
+
+    },
+    {
+        path: "/about",
+        element: <About />,
+
+
+    }
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />)
